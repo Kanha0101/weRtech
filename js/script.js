43a16fac6149c09d94ec13f35410bce5de545d3c
Itz.Shreyash <125800747+Kanha0101@users.noreply.github.com>
2024-03-03 21:59:42 +0530

    font

--- js/script.js
+++ js/script.js
@@ -1,28 +1,43 @@
-Shery.mouseFollower({
-    //Parameters are optional.
-    skew: true,
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
+var tl = gsap.timeline()
 
-gsap.from(".anime", {
-    stagger: .2,
-    y: 10,
-    duration: 1,
-    ease: Power2
+
+
+tl.from("#loader h3", {
+    x:40,
+    opacity:0,
+    duration:1,
+    stagger:0.1
 })
 
-gsap.from("#subscribe>h1", {
+tl.to("#loader h3", {
+    opacity:0,
+    x:-40,
+    duration:1,
+    stagger:0.1
+})
+
+tl.to("#loader", {
+    opacity:0
+})
+
+
+tl.from("#wertech h1 span", {
+    y:10,
+    opacity:0,
+    duration:0.5,
+    stagger:0.1,
+    delay:-0.3
+})
+
+tl.from(".anime", {
     stagger: .2,
+    opacity:0,
     y: 10,
     duration: 1,
     ease: Power2,
-    opacity: 0
+    delay:-0.5
 })
-gsap.from("#subscribe #wertech h1", {
-    stagger: .2,
-    y: 20,
-    duration: 1,
-    ease: Power3,
-    opacity: 0
+
+tl.to("#loader", {
+    display:"none"
 })
